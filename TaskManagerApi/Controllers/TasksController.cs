061fac048f380159ebca67606cee860c02e1587e
suba444raj@gmail.com <142592205+suba18rajan@users.noreply.github.com>
2026-05-24 16:29:20 +0530

    Create PUT update task API

--- TaskManagerApi/Controllers/TasksController.cs
+++ TaskManagerApi/Controllers/TasksController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using TaskManagerApi.Data;
 using TaskManagerApi.Models;
+using System.Linq;
 
 namespace TaskManagerApi.Controllers
 {
@@ -20,6 +21,22 @@ namespace TaskManagerApi.Controllers
             TaskData.Tasks.Add(newTask);
 
             return Ok(newTask);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateTask(int id, TaskItem updatedTask)
+        {
+            var existingTask = TaskData.Tasks.FirstOrDefault(x => x.Id == id);
+
+            if (existingTask == null)
+            {
+                return NotFound("Task not found");
+            }
+
+            existingTask.Title = updatedTask.Title;
+            existingTask.IsCompleted = updatedTask.IsCompleted;
+
+            return Ok(existingTask);
         }
     }
 }
