f4519bb424d9c71dd9b5dce8549957e440dc31c2
suba444raj@gmail.com <142592205+suba18rajan@users.noreply.github.com>
2026-07-26 19:01:44 +0530

    Add DTO validation using Data Annotations

--- TaskManagerApi/DTOs/TaskCreateDto.cs
+++ TaskManagerApi/DTOs/TaskCreateDto.cs
@@ -1,7 +1,11 @@
-﻿namespace TaskManagerApi.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskManagerApi.DTOs
 {
     public class TaskCreateDTO
     {
+        [Required(ErrorMessage = "Title is required.")]
+        [StringLength(100, ErrorMessage = "Title cannot exceed 100 characters.")]
         public string Title { get; set; }
     }
 }
--- TaskManagerApi/DTOs/TaskUpdateDto.cs
+++ TaskManagerApi/DTOs/TaskUpdateDto.cs
@@ -1,8 +1,13 @@
-﻿namespace TaskManagerApi.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskManagerApi.DTOs
 {
     public class TaskUpdateDTO
     {
+        [Required(ErrorMessage = "Title is required.")]
+        [StringLength(100, ErrorMessage = "Title cannot exceed 100 characters.")]
         public string Title { get; set; }
+
         public bool IsCompleted { get; set; }
     }
 }
